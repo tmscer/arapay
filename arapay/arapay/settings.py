382d2765477498a7c56c71b54f32dc46e33f9759
Tomáš Černý <tmscer@gmail.com>
2018-09-04 09:55:28 +0200

    Payments models + migration.

--- arapay/arapay/settings.py
+++ arapay/arapay/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', 'abydos', 'avava.gyarab.cz']
 # Application definition
 
 INSTALLED_APPS = [
+    'payments.apps.PaymentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- arapay/payments/models.py
+++ arapay/payments/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Invoice(models.Model):
+    date_added = models.fields.DateTimeField('date added')
+    date_deadine = models.fields.DateTimeField('date due')
+    payment_group = models.fields.TextField('payment group', max_length=64)
+    description = models.fields.TextField('payment description', max_length=200)
+    amount_cents = models.fields.BigIntegerField('amount in cents')
+
+
+class Payment(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.fields.BigIntegerField('amount in cents')
