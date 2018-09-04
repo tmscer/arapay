650e3fa546dd8de161930e298c87f6c5a86df15e
Tomáš Černý <tmscer@gmail.com>
2018-09-04 10:00:11 +0200

    models field renames

--- arapay/payments/models.py
+++ arapay/payments/models.py
@@ -7,13 +7,17 @@ User = get_user_model()
 
 class Invoice(models.Model):
     date_added = models.fields.DateTimeField('date added')
-    date_deadine = models.fields.DateTimeField('date due')
+    date_deadline = models.fields.DateTimeField('date due')
     payment_group = models.fields.TextField('payment group', max_length=64)
     description = models.fields.TextField('payment description', max_length=200)
     amount_cents = models.fields.BigIntegerField('amount in cents')
+
+    def __repr__(self):
+        return "Invoice(date_added={}, date_deadline={}, payment_group={}, description={}, amount_cents={})" \
+            .format(self.date_added, self.date_deadline, self.payment_group, self.description, self.amount_cents)
 
 
 class Payment(models.Model):
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    amount = models.fields.BigIntegerField('amount in cents')
+    amount_cents = models.fields.BigIntegerField('amount in cents')
