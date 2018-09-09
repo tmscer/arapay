8a4f396a7e3344db52b2d44c4780d116e1be6984
Tomáš Černý <tmscer@gmail.com>
2018-09-09 08:12:00 +0200

    view readability

--- arapay/payments/views.py
+++ arapay/payments/views.py
@@ -6,48 +6,47 @@ from payments.serializers import InvoiceSerializer, PaymentSerializer
 
 
 def index(request):
-    if request.user.is_authenticated:
-        groups = request.user.groups.all().values()
-        group_ids = [g['id'] for g in groups]
-
-        invoices_paid = []
-        invoices_unpaid = []
-        invoices_overpaid = []
-
-        invoices_result = Invoice.objects \
-            .filter(groups__in=group_ids) \
-            .distinct() \
-            .values('id', 'name', 'description', 'date_added', 'date_deadline', 'amount_cents')
-
-        for invoice in invoices_result:
-            payment = Payment.objects \
-                .filter(invoice_id=invoice['id'], user_id=request.user.id) \
-                .values().first()
-            if payment is None:
-                invoice['payment'] = {
-                    'amount_cents': 0,
-                    'user_id': request.user.id
-                }
-                invoices_unpaid.append(invoice)
-                continue
-            invoice['payment'] = payment
-            if invoice['amount_cents'] > payment['amount_cents']:
-                invoices_unpaid.append(invoice)
-            elif invoice['amount_cents'] == payment['amount_cents']:
-                invoices_paid.append(invoice)
-            else:
-                invoices_overpaid.append(invoice)
-
-        data = {'username': request.user.email,
-                'invoices': {
-                    'paid': invoices_paid,
-                    'unpaid': invoices_unpaid,
-                    'overpaid': invoices_overpaid,
-                },
-                'groups': list(groups)}
-        return render(request, 'payments/invoices.html', data)
-    else:
+    if not request.user.is_authenticated:
         return render(request, 'payments/notloggedin.html')
+    groups = request.user.groups.all().values()
+    group_ids = [g['id'] for g in groups]
+
+    invoices_paid = []
+    invoices_unpaid = []
+    invoices_overpaid = []
+
+    invoices_result = Invoice.objects \
+        .filter(groups__in=group_ids) \
+        .distinct() \
+        .values('id', 'name', 'description', 'date_added', 'date_deadline', 'amount_cents')
+
+    for invoice in invoices_result:
+        payment = Payment.objects \
+            .filter(invoice_id=invoice['id'], user_id=request.user.id) \
+            .values().first()
+        if payment is None:
+            invoice['payment'] = {
+                'amount_cents': 0,
+                'user_id': request.user.id
+            }
+            invoices_unpaid.append(invoice)
+            continue
+        invoice['payment'] = payment
+        if invoice['amount_cents'] > payment['amount_cents']:
+            invoices_unpaid.append(invoice)
+        elif invoice['amount_cents'] == payment['amount_cents']:
+            invoices_paid.append(invoice)
+        else:
+            invoices_overpaid.append(invoice)
+
+    data = {'username': request.user.email,
+            'invoices': {
+                'paid': invoices_paid,
+                'unpaid': invoices_unpaid,
+                'overpaid': invoices_overpaid,
+            },
+            'groups': list(groups)}
+    return render(request, 'payments/invoices.html', data)
 
 
 # REST API VIEWS
