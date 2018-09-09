e5c0bd5b5335fbf5b1d9bb7e3e5ef06611f4b149
Tomáš Černý <tmscer@gmail.com>
2018-09-09 14:02:49 +0200

    delegated getting of invoices to a helper

--- arapay/payments/views.py
+++ arapay/payments/views.py
@@ -4,46 +4,17 @@ from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from payments import helpers
 from payments.models import Invoice, Payment
 from payments.serializers import InvoiceSerializer, PaymentSerializer
 
 
 def index(request):
     if not request.user.is_authenticated:
-        return render(request, 'payments/notloggedin.html')
+        return render(request, 'payments/base.html')
     groups = request.user.groups.all().values()
-    group_ids = [g['id'] for g in groups]
 
-    invoices_paid = []
-    invoices_unpaid = []
-    invoices_overpaid = []
-
-    invoices_result = Invoice.objects \
-        .filter(groups__in=group_ids) \
-        .distinct() \
-        .values('id', 'name', 'description', 'date_added', 'date_deadline', 'amount_cents')
-
-    for invoice in invoices_result:
-        payment = Payment.objects \
-            .filter(invoice_id=invoice['id'], user_id=request.user.id) \
-            .values().first()
-        if payment is None:
-            invoice['payment'] = {
-                'amount_cents': 0,
-                'user_id': request.user.id
-            }
-            if invoice['amount_cents'] < 0:
-                invoices_overpaid.append(invoice)
-            else:
-                invoices_unpaid.append(invoice)
-            continue
-        invoice['payment'] = payment
-        if invoice['amount_cents'] > payment['amount_cents']:
-            invoices_unpaid.append(invoice)
-        elif invoice['amount_cents'] == payment['amount_cents']:
-            invoices_paid.append(invoice)
-        else:
-            invoices_overpaid.append(invoice)
+    invoices_paid, invoices_unpaid, invoices_overpaid = helpers.invoices_paid_unpaid_overpaid(request, request.user)
 
     data = {'user': request.user,
             'invoices': {
