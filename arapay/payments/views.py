6db67340a1ad3321b148cee90b00c28af3ab742d
Tomáš Černý <tmscer@gmail.com>
2018-09-09 09:56:35 +0200

    var_symbol generation + account_number as var

--- arapay/payments/views.py
+++ arapay/payments/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -47,14 +49,50 @@ def index(request):
                 'unpaid': invoices_unpaid,
                 'overpaid': invoices_overpaid,
             },
-            'groups': list(groups)}
+            'groups': list(groups),
+            'account_number': '285621010/0300'}
     return render(request, 'payments/invoices.html', data)
 
 
 @login_required
 def generate_var_symbol(request, invoice_id):
-    invoice = Invoice.objects.filter(id=invoice_id)
-    return HttpResponse(invoice)
+    groups = request.user.groups.all().values()
+    group_ids = [g['id'] for g in groups]
+    invoice_result = Invoice.objects.filter(id=invoice_id, groups__in=group_ids)
+    if len(invoice_result) != 0:
+        invoice = invoice_result.get()
+    else:
+        return HttpResponse('{"error":"invoice.na"}')
+
+    payment_query = Payment.objects.filter(invoice_id=invoice_id, user_id=request.user.id)
+    if len(payment_query) == 0:
+        payment = None
+    else:
+        payment = payment_query.get()
+
+    def gen_var_symbol():
+        vs = random.randint(100000, 999999)
+        while len(Payment.objects.filter(var_symbol=vs)) > 0:
+            vs = random.randint(100000, 999999)
+        return vs
+
+    if payment is None:
+        # Create it
+        payment = Payment.objects.create(invoice_id=invoice_id,
+                                         user_id=request.user.id,
+                                         amount_cents=0,
+                                         var_symbol=gen_var_symbol())
+    elif payment.amount_cents == invoice.amount_cents:
+        return HttpResponse('{"error":"paid"}')
+    elif payment.amount_cents > invoice.amount_cents:
+        return HttpResponse('{"error":"overpaid"}')
+    elif payment.var_symbol != 0:
+        return HttpResponse('{"error":"var_symbol.exists"}')
+    else:
+        # Var symbol has length 6
+        payment.var_symbol = gen_var_symbol()
+        payment.save()
+    return HttpResponse('{"var_symbol":%s}' % payment.var_symbol)
 
 
 # REST API VIEWS
