66d22d44daa5ff899c028feff09c1178b76b603a
Tomáš Černý <tmscer@gmail.com>
2018-10-21 19:52:01 +0200

    csrf token = false on dev

--- arapay/arapay/settings.py
+++ arapay/arapay/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Security changes thanks to `manage.py check --deploy`
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 X_FRAME_OPTIONS = 'DENY'
 
 # Application definition
