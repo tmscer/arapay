53f6d299a7dede5af292bbfb80af7495ad23358e
Tomáš Černý <tmscer@gmail.com>
2019-03-10 09:37:50 +0100

    select invoices by user

--- arapay/payments/forms.py
+++ arapay/payments/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
-from payments.models import Invoice
+from payments.models import Invoice, User
 
 
-class InvoiceSearchForm(forms.Form):
+class InvoiceSelectForm(forms.Form):
     invoice_select = forms.ModelChoiceField(Invoice.objects.order_by('-date_added'))
+
+
+class UserSelectForm(forms.Form):
+    user_select = forms.ModelChoiceField(User.objects.order_by('-last_name'))
